53e4d4f95dff4d6212fe866e58d826b256fb4725
greg <elorian.mestec@gmail.com>
2015-01-18 15:48:33 -0800

    Working hello world Websockets server

--- voluspa-server.hs
+++ voluspa-server.hs
@@ -1,6 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Network.WebSockets as WS
+import qualified Network.WebSockets as WS
+import qualified Data.Text as T
+import Control.Monad (forever)
 
-main = do
-    putStrLn "Yo"
+
+echo :: WS.Connection -> IO ()
+echo conn = forever $ do
+    msg <- WS.receiveData conn
+    WS.sendTextData conn (T.append msg " meow")
+
+port :: Int
+port = 22000
+
+application :: WS.PendingConnection -> IO ()
+application pend = do
+    conn <- WS.acceptRequest pend
+    echo conn
+
+main = WS.runServer "0.0.0.0" port application
