1101f06f3557164583c2627c0928de99bfca99a9
Alex Nisnevich <alex.nisnevich@gmail.com>
2015-01-29 00:41:39 -0800

    defining some types

--- voluspa-server.hs
+++ voluspa-server.hs
@@ -5,8 +5,20 @@ import Control.Monad
 import Data.Aeson
 import Data.Aeson.Types
 import Data.ByteString.Lazy.Internal
+import Data.HashMap.Lazy
 import qualified Data.Text as T
 import qualified Network.WebSockets as WS
+
+data ServerState = ServerState ClientsMap GamesMap WaitingClient
+
+type ClientsMap = HashMap ClientId WS.Connection
+type GamesMap = HashMap GameId Game
+type WaitingClient = Maybe ClientId
+
+data Game = Game ClientId ClientId
+
+type ClientId = String
+type GameId = String
 
 data Action = 
   Action { action :: !T.Text } deriving Show
@@ -18,9 +30,9 @@ instance FromJSON Action where
 
 echo :: WS.Connection -> IO ()
 echo conn = forever $ do
-    msg <- WS.receiveData conn
-    -- WS.sendTextData conn ((action (decodeAction msg)) :: T.Text)
-    WS.sendTextData conn (msg :: T.Text)
+  msg <- WS.receiveData conn
+  -- WS.sendTextData conn ((action (decodeAction msg)) :: T.Text)
+  WS.sendTextData conn (msg :: T.Text)
 
 decodeAction :: ByteString -> Action
 decodeAction msg = 
@@ -35,7 +47,7 @@ port = 22000
 
 application :: WS.PendingConnection -> IO ()
 application pend = do
-    conn <- WS.acceptRequest pend
-    echo conn
+  conn <- WS.acceptRequest pend
+  echo conn
 
 main = WS.runServer "0.0.0.0" port application
