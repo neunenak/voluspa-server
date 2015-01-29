835f34ac25d3c1a857ff612b8f57848f67a60da8
Alex Nisnevich <alex.nisnevich@gmail.com>
2015-01-29 00:24:44 -0800

    correctly parsing action types

--- voluspa-server.hs
+++ voluspa-server.hs
@@ -1,14 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import qualified Network.WebSockets as WS
+import Control.Applicative
+import Control.Monad
+import Data.Aeson
+import Data.Aeson.Types
+import Data.ByteString.Lazy.Internal
 import qualified Data.Text as T
-import Control.Monad (forever)
+import qualified Network.WebSockets as WS
 
+data Action = 
+  Action { action :: !T.Text } deriving Show
+
+instance FromJSON Action where
+  parseJSON (Object v) =
+    Action <$> v .: "action"
+  parseJSON _ = mzero
 
 echo :: WS.Connection -> IO ()
 echo conn = forever $ do
     msg <- WS.receiveData conn
-    WS.sendTextData conn (T.append msg " meow")
+    -- WS.sendTextData conn ((action (decodeAction msg)) :: T.Text)
+    WS.sendTextData conn (msg :: T.Text)
+
+decodeAction :: ByteString -> Action
+decodeAction msg = 
+  let d = eitherDecode msg :: Either String Action
+  in
+    case d of
+      Left err -> Action {action = "NoAction"}
+      Right action -> action
 
 port :: Int
 port = 22000
