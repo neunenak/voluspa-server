2fe5614a19fc7c3661004d83676a296feb4e3ef4
Alex Nisnevich <alex.nisnevich@gmail.com>
2015-02-23 21:43:01 -0800

    player names

--- voluspa-server.hs
+++ voluspa-server.hs
@@ -20,19 +20,23 @@ import System.Random
 
 data ServerState = ServerState ClientsMap GamesMap WaitingClient deriving Show
 
-type ClientsMap = HashMap ClientId WS.Connection
+type ClientsMap = HashMap ClientId Client
 type GamesMap = HashMap ClientId ClientId   -- map of player to opponent (two entries per matchup). Maybe GamesMap is the wrong name for this?
 type WaitingClient = Maybe ClientId
 
+data Client = Client ClientName WS.Connection deriving Show
+
+type ClientName = T.Text
 type ClientId = Int
 
 data Message =
-  Message { action :: !T.Text }
+  Message { action :: !T.Text, playerName :: !T.Text }
   deriving Show
 
 instance FromJSON Message where
   parseJSON (Object v) =
     Message <$> v .: "action"
+            <*> v .:? "playerName" .!= ""
   parseJSON _ = mzero
 
 instance Show WS.Connection where
@@ -49,27 +53,27 @@ decodeMessage msg =
   let d = eitherDecode msg :: Either String Message
   in
     case d of
-      Left err -> Message {action = "NoAction"}
+      Left err -> Message {action = "NoAction", playerName = ""}
       Right action -> action
 
-addWaitingClient :: WS.Connection -> ClientId -> ServerState -> ServerState
-addWaitingClient conn clientId (ServerState clients games _) =
+addWaitingClient :: WS.Connection -> ClientId -> ClientName -> ServerState -> ServerState
+addWaitingClient conn clientId clientName (ServerState clients games _) =
   -- First remove any matchups the client is currently in, then set the client as the waiting client
   let gamesWithClientRemoved = HM.filter (\oppId -> oppId /= clientId) $ HM.delete clientId games
   in
-    ServerState (insert clientId conn clients) gamesWithClientRemoved (Just clientId)
+    ServerState (insert clientId (Client clientName conn) clients) gamesWithClientRemoved (Just clientId)
 
-matchClients :: WS.Connection -> ClientId -> ServerState -> ServerState
-matchClients conn clientId (ServerState clients games (Just waitingClientId)) =
+matchClients :: WS.Connection -> ClientId -> ClientName -> ServerState -> ServerState
+matchClients conn clientId clientName (ServerState clients games (Just waitingClientId)) =
   -- First remove any matchups the client is currently in, then create a new matchup
   -- between the client and the waiting client
   let gamesWithClientRemoved = HM.filter (\oppId -> oppId /= clientId) $ HM.delete clientId games
       newGames = insert waitingClientId clientId $ insert clientId waitingClientId gamesWithClientRemoved
   in
-    ServerState (insert clientId conn clients) newGames Nothing
+    ServerState (insert clientId (Client clientName conn) clients) newGames Nothing
 
-findMatchingConnection :: ClientId -> ServerState -> Maybe WS.Connection
-findMatchingConnection clientId (ServerState clients games waitingClient) =
+findMatchingClient :: ClientId -> ServerState -> Maybe Client
+findMatchingClient clientId (ServerState clients games waitingClient) =
   let otherClientId = HM.lookup clientId games
   in liftM (\cId -> clients ! cId) otherClientId
 
@@ -84,7 +88,7 @@ response :: WS.Connection -> ClientId -> MVar ServerState -> IO ()
 response conn clientId mvarState = forever $ do
   clientMsg <- WS.receiveData conn
   let clientMsgText = T.pack $ Char8.unpack $ BS.concat $ BSL.toChunks clientMsg
-  let message@(Message action) = decodeMessage clientMsg
+  let message@(Message action playerName) = decodeMessage clientMsg
   putStrLn (show (clientId, clientMsgText))
 
   state <- liftIO $ readMVar mvarState
@@ -95,34 +99,36 @@ response conn clientId mvarState = forever $ do
 
       -- If the client is currently in a game, let the opponent know that this client disconnected
       -- TODO: this duplicates code from handleDisconnect - extract into a separate method?
-      let maybeOpponentConn = findMatchingConnection clientId state
-      when (isJust maybeOpponentConn) $
-        WS.sendTextData (fromJust maybeOpponentConn) ("{\"action\":\"OpponentDisconnected\"}" :: T.Text) -- TODO: do this through Aeson
+      let maybeOpponentClient = findMatchingClient clientId state
+      when (isJust maybeOpponentClient) $
+        let Client _ opponentConn = (fromJust maybeOpponentClient)
+        in WS.sendTextData opponentConn ("{\"action\":\"OpponentDisconnected\"}" :: T.Text) -- TODO: do this through Aeson
 
       case waitingClient of
         Just id | id == clientId -> return ()  -- If client is already waiting for a game, continue waiting
 
         Just opponentId -> liftIO $ modifyMVar_ mvarState $ \s -> do
-          let newState@(ServerState clientMap _ _) = matchClients conn clientId s
-          let opponentConn = clientMap ! opponentId
+          let newState@(ServerState clientMap _ _) = matchClients conn clientId playerName s
+          let Client opponentName opponentConn = clientMap ! opponentId
 
           -- Assign the current player and opponent the colors Red and Blue, respectively,
           -- and pass on the StartGame messages.
           -- TODO: do this through Aeson rather than this ridiculous find-and-replace approach
-          WS.sendTextData conn (T.replace "}" ", \"color\":\"red\"}" clientMsgText)
-          WS.sendTextData opponentConn (T.replace "}" ", \"color\":\"blue\"}" clientMsgText)
+          WS.sendTextData conn (T.replace "}" (T.concat [",\"color\":\"red\",\"opponentName\":\"", opponentName, "\"}"]) clientMsgText)
+          WS.sendTextData opponentConn (T.replace "}" (T.concat [",\"color\":\"blue\",\"opponentName\":\"", playerName, "\"}"]) clientMsgText)
 
           putStrLn $ show newState
           return newState
 
         Nothing -> liftIO $ modifyMVar_ mvarState $ \s -> do
-          let newState = addWaitingClient conn clientId s
+          let newState = addWaitingClient conn clientId playerName s
           putStrLn $ show newState
           return newState
     _ ->
-      let maybeOpponentConn = findMatchingConnection clientId state
-      in when (isJust maybeOpponentConn) $
-        WS.sendTextData (fromJust maybeOpponentConn) clientMsgText
+      let maybeOpponentClient = findMatchingClient clientId state
+      in when (isJust maybeOpponentClient) $
+        let Client _ opponentConn = (fromJust maybeOpponentClient)
+        in WS.sendTextData opponentConn clientMsgText
 
 
 handleDisconnect :: WS.Connection -> ClientId -> MVar ServerState -> WS.ConnectionException -> IO ()
@@ -131,9 +137,10 @@ handleDisconnect conn clientId mvarState exception = do
 
   state <- liftIO $ readMVar mvarState
 
-  let maybeOpponentConn = findMatchingConnection clientId state
-  when (isJust maybeOpponentConn) $
-    WS.sendTextData (fromJust maybeOpponentConn) ("{\"action\":\"OpponentDisconnected\"}" :: T.Text) -- TODO: do this through Aeson
+  let maybeOpponentClient = findMatchingClient clientId state
+  when (isJust maybeOpponentClient) $
+    let Client _ opponentConn = (fromJust maybeOpponentClient)
+    in WS.sendTextData opponentConn ("{\"action\":\"OpponentDisconnected\"}" :: T.Text) -- TODO: do this through Aeson
 
   liftIO $ modifyMVar_ mvarState $ \s -> do
     let newState = removeClient clientId s
